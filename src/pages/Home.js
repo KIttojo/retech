a46ed503e14c840906739117847381fcbad0fd4e
KIttojo <denis_fedorenko_2015@mail.ru>
2022-05-29 14:31:06 +0300

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
 import Contact from "../components/Contact/Contact";
-import Footer from "../components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -10,7 +9,6 @@ const Home = () => {
       <Hero />
       <Projects />
       <Contact />
-      <Footer />
     </>
   );
 }
